38e2f3baaa52f0b3b149ccaff15cf9e18f9cb3ab
vrashi <vrashrivastava@gmail.com>
2024-07-27 10:24:42 -0400

    initial commit

---
+++ app.py
@@ -0,0 +1,240 @@
+import streamlit as st
+import pandas as pd
+from PIL import Image
+
+def main():
+    
+    
+    
+    def write_to_csv(name, add_line_1, add_line_2, city, state, zip, phone_number):
+    # Define CSV file path
+        csv_file_path = "Customer_Details.csv"
+
+        # Create a DataFrame or load existing
+        try:
+            df = pd.read_csv(csv_file_path)
+        except FileNotFoundError:
+            df = pd.DataFrame(columns=["name", "add_line_1", "add_line_2", "city", "state", "ZIP", "phone_number"])
+
+        # Add a new row with the submitted values
+        new_row = {"name": name, "add_line_1": add_line_1, "add_line_2":add_line_2, "city":city, "state":state, "ZIP":zip, "phone_number":phone_number}
+        df = df._append(new_row, ignore_index=True)
+
+        # Write DataFrame to CSV
+        df.to_csv(csv_file_path, index=False)
+
+    # def display_book(book_title, book_image, book_description):
+    #     st.header("Mark Zak", divider='rainbow')
+    #     st.title(book_title)
+        
+    #     st.image(book_image, width=300)
+    #     st.write("#### Who would benefit from this study:")
+    #     st.markdown(
+    #             f"""
+    #             <div style="width: 100%; word-wrap: break-word;">
+    #                 {book_description}
+    #             </div>
+    #             """,
+    #             unsafe_allow_html=True
+    #         )
+
+    
+    
+    
+    
+
+    def book1():
+        # book1desc = "Most importantly, every chord in each song example should be spelled with 100% accuracy. This is critical to identifying the active tones and hearing with clarity the harmonic meaning of each. Then, every harmonic function and substitution must be studied and understood by playing and listening to the individual notes in the examples provided. By doing that, occurrences of the chord functions will become increasingly noticeable when hearing music. Please note, these are typical progressions from actual popular songs; these chords and harmonic principles are evident in countless other songs. Rarely is it otherwise. Instrumentalists can practice these progressions, employing proper voicing, as these are actual progressions from popular songs, and have value beyond their pedagogical utility. Vocalists can determine which chord tones can provide a basis for a harmonizing part. Improvisers can freely invent their own melodies. Composers and arrangers can use the chord functions and progressions provided by the chart of Normal Harmonic Progression for ideas and suggestions on how to substitute or expand chords. Serious listeners and music lovers can increase their musical consciousness and appreciation of all harmonically based music in general, regardless of time period or style. Every worthwhile endeavor requires repeated application of effort, and every musician (or athlete, scientist, writer, builder, etc.) knows that. The study of harmony will reveal surprising insights, so be patient, and take it one half step at a time."
+
+        # display_book("Popular Song Harmony Explained: A Guide to Hearing Chord Vectors", "popular_song_harmony_cover.png", book1desc)
+        # Second Container: Last Week Stats
+        st.header("Michael Phillip Zak", divider='rainbow')
+        st.title("Popular Song Harmony Explained: A Guide to Hearing Chord Vectors")
+      
+        st.image("popular_song_harmony_cover.png", width = 300)
+        st.write("#### Who would benefit from this study:")
+        st.write("Anyone involved with chords and chord progressions, including music students, prospective improvisers, arrangers, composers, singers, and serious listeners with an interest in the nuts and bolts of how musical harmony works.")
+        st.write("#### What it aims to do:")
+        st.write("Provide a framework for hearing the meaning of individual chords as they relate to the overall goal of a progression.")
+        st.write("#### How to use it:")
+        st.write("Most importantly, every chord in each song example should be spelled with 100% accuracy. This is critical to identifying the active tones and hearing with clarity the harmonic meaning of each. Then, every harmonic function and substitution must be studied and understood by playing and listening to the individual notes in the examples provided. By doing that, occurrences of the chord functions will become increasingly noticeable when hearing music. Please note, these are typical progressions from actual popular songs; these chords and harmonic principles are evident in countless other songs. Rarely is it otherwise.")
+        st.write("Instrumentalists can practice these progressions, employing proper voicing, as these are actual progressions from popular songs, and have value beyond their pedagogical utility. Vocalists can determine which chord tones can provide a basis for a harmonizing part. Improvisers can freely invent their own melodies. Composers and arrangers can use the chord functions and progressions provided by the chart of Normal Harmonic Progression for ideas and suggestions on how to substitute or expand chords. Serious listeners and music lovers can increase their musical consciousness and appreciation of all harmonically based music in general, regardless of time period or style.")
+        st.write("Every worthwhile endeavor requires repeated application of effort, and every musician (or athlete, scientist, writer, builder, etc.) knows that. The study of harmony will reveal surprising insights, so be patient, and take it one half step at a time.")
+
+        st.header("Place The Order")
+
+        with st.form(key='user_form'):
+            name = st.text_input('Name')
+            add_line_1 = st.text_input('Address Line 1')
+            add_line_2 = st.text_input('Address Line 2')
+            city = st.text_input('City')
+            state = st.text_input('State')
+            zip = st.text_input('Zip Code')
+            phone_number = st.text_input('Phone Number')
+            submit_button = st.form_submit_button(label='Buy')
+
+    # name = st.text_input("Name*", "Enter full name")
+    # add_line_1 = st.text_input("Address line 1*", "Enter address")
+    # add_line_2 = st.text_input("Address line 2", "Enter address")
+    # city = st.text_input("City*", "E.g. Buffalo")
+    # state = st.text_input("State*", "E.g. NY")
+    # zip = st.text_input("Zip Code*", "E.g. 14214")
+    # phone_number = st.text_input("Phone Number*", "Enter with the country code E.g. +1716....")
+    # print(client_name)
+    # print(property_id)
+    # print(website_name)
+        
+
+
+    # Define the URL you want to redirect to
+        url = "https://buy.stripe.com/test_00geWGc543wXg6s3cc"
+
+
+        if submit_button:
+            if name == "Enter full name" or name == "":
+                st.error("Enter your name")
+            elif add_line_1 == "Enter address" or add_line_1 == "":
+                st.error("Enter your address")
+            elif city == "E.g. Buffalo" or city == "":
+                st.error("Enter your city")
+            elif state == "E.g. NY" or state == "":
+                st.error("Enter your state")
+            elif zip == "E.g. 14214" or zip == "":
+                st.error("Enter your ZIP code")
+            elif phone_number == "Enter with the country code E.g. +1716...." or phone_number == "":
+                st.error("Enter your phone number")
+            else:
+                write_to_csv(name, add_line_1, add_line_2, city, state, zip, phone_number)
+                st.markdown(f"""
+                    <meta http-equiv="refresh" content="0; url={url}">
+                """, unsafe_allow_html=True)
+
+    def book2():
+
+    
+        # Second Container: Last Week Stats
+        st.header("Michael Phillip Zak", divider='rainbow')
+        st.title("Musical Chord Symbols and Symmetrical Polytonality: Hidden Harmonic Dimensions")
+        st.image("musical_chord_symbols_cover.png", width = 300)
+        st.write("#### What is the point of music theory and the study of harmony?")
+        st.write("It clarifies by ear colorful textures and meaningful tensions that the harmony expresses. By identifying the underlying structures and hidden forces the mind lights up as well as the heart. If you know what to look for, the ear is prepared to find it, and remember it. Finding it repeatedly in unexpected places enriches the entire listening experience.")
+        st.write("It provides a path of hearing through the countless relationships of tones in order to make them conscious, and therefore useful to the composer, improviser, arranger, and deep listener.")
+        st.write("What are these powerful hidden natural forces that affects virtually every person in such a way? The study of harmony attempt to reveal these forces, and make them visible. To see the underlying structure of nature and the working elements that exist in the hidden dimensions of music can only be a good thing. Can our systems of understanding be limited? That goes without saying. Do they reveal something? Yes, and the musical test is to hear it, which is a reward in itself.")
+        st.write("#### What are the problems of music theory description?")
+        st.write("Explanation is a larger concept than definition. We define the elements, then explain their relationships. Relationships occur in levels of influence, some more fundamental than others. Analysis attempts to explain the levels of relationships.")
+        st.write("Describing a multidimensional set of relationships using the linear structure of language is the primary problem of music theory. The whole must be grasped to see the relationship of the parts.")
+        st.write("Another problem is that the notational system of seven letter names attached to twelve individual pitches is unwieldy when applied to symmetrical relationships. Having a solid command of enharmonic chord spelling is absolutely essential.")
+        st.write("#### The path is easy to outline:")
+        st.write("Spell the chord with perfect accuracy.")
+        st.write("Identify the internal parts that give it directional motion.")
+        st.write("Play the progression with smooth voice leading.")
+        st.write("Listen to the progression and verify the tendency tones by ear.")
+        
+        st.header("Place The Order")
+
+        with st.form(key='user_form'):
+            name = st.text_input('Name')
+            add_line_1 = st.text_input('Address Line 1')
+            add_line_2 = st.text_input('Address Line 2')
+            city = st.text_input('City')
+            state = st.text_input('State')
+            zip = st.text_input('Zip Code')
+            phone_number = st.text_input('Phone Number')
+            submit_button = st.form_submit_button(label='Buy')
+
+    # name = st.text_input("Name*", "Enter full name")
+    # add_line_1 = st.text_input("Address line 1*", "Enter address")
+    # add_line_2 = st.text_input("Address line 2", "Enter address")
+    # city = st.text_input("City*", "E.g. Buffalo")
+    # state = st.text_input("State*", "E.g. NY")
+    # zip = st.text_input("Zip Code*", "E.g. 14214")
+    # phone_number = st.text_input("Phone Number*", "Enter with the country code E.g. +1716....")
+    # print(client_name)
+    # print(property_id)
+    # print(website_name)
+        
+
+
+    # Define the URL you want to redirect to
+        url = "https://buy.stripe.com/test_00geWGc543wXg6s3cc"
+
+
+        if submit_button:
+            if name == "Enter full name" or name == "":
+                st.error("Enter your name")
+            elif add_line_1 == "Enter address" or add_line_1 == "":
+                st.error("Enter your address")
+            elif city == "E.g. Buffalo" or city == "":
+                st.error("Enter your city")
+            elif state == "E.g. NY" or state == "":
+                st.error("Enter your state")
+            elif zip == "E.g. 14214" or zip == "":
+                st.error("Enter your ZIP code")
+            elif phone_number == "Enter with the country code E.g. +1716...." or phone_number == "":
+                st.error("Enter your phone number")
+            else:
+                write_to_csv(name, add_line_1, add_line_2, city, state, zip, phone_number)
+                st.markdown(f"""
+                    <meta http-equiv="refresh" content="0; url={url}">
+                """, unsafe_allow_html=True)
+
+    # Create a button
+    # if st.button('Buy query param'):
+    #     # Redirect the user
+    #     if name == "Enter full name" or name == "":
+    #         st.error("Enter your name")
+    #     elif add_line_1 == "Enter address" or add_line_1 == "":
+    #         st.error("Enter your address")
+    #     elif city == "E.g. Buffalo" or city == "":
+    #         st.error("Enter your city")
+    #     elif state == "E.g. NY" or state == "":
+    #         st.error("Enter your state")
+    #     elif zip == "E.g. 14214" or zip == "":
+    #         st.error("Enter your ZIP code")
+    #     elif phone_number == "Enter with the country code E.g. +1716...." or phone_number == "":
+    #         st.error("Enter your phone number")
+    #     else:
+    #         write_to_csv(name, add_line_1, add_line_2, city, state, zip, phone_number)
+    #         st.experimental_set_query_params(redirect_url=url)
+
+    # # Check if there is a query parameter for redirection
+    # query_params = st.experimental_get_query_params()
+    # if 'redirect_url' in query_params:
+    #     redirect_url = query_params['redirect_url'][0]
+    #     # Execute JavaScript to redirect the user
+    #     st.write(f"""
+    #         <meta http-equiv="refresh" content="0; url={redirect_url}">
+    #     """, unsafe_allow_html=True)
+
+
+    # st.markdown(
+        
+    #     """
+    #     <a href="https://buy.stripe.com/test_00geWGc543wXg6s3cc" target="_blank">
+    #         <button style="background-color: #36454F; border: none; color: white; padding: 15px 32px; text-align: center; text-decoration: none; display: inline-block; font-size: 16px; margin: 4px 2px; cursor: pointer; border-radius: 8px;">
+    #             Buy markdown
+    #         </button>
+    #     </a>
+    #     """,
+    #     unsafe_allow_html=True
+    # )
+            
+
+    st.sidebar.title("Books Available")
+    selection = st.sidebar.radio("Go to", ["Popular Song Harmony Explained", "Musical Chord Symbols and Symmetrical Polytonality"])
+    
+
+    # Display the appropriate page based on user selection
+    if selection == "Popular Song Harmony Explained":
+        book1()
+    else:
+        book2()
+
+   
+
+    
+    
+    
+
+if __name__ == "__main__":
+    main()
